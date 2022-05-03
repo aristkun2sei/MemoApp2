ada6c0aa2efeef90e8bdcbf053d5895d33b9b3df
taichi <103873740+aristkun2sei@users.noreply.github.com>
2022-05-04 02:36:20 +0900

    Install @expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+//import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoListScreen />
   );
 }
 
