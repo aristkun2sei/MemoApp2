0607b5fa24d7ed9f19a6049f8ac5ce51fbd23587
taichi <103873740+aristkun2sei@users.noreply.github.com>
2022-05-04 20:32:11 +0900

    Make components touchable

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    View, Text, StyleSheet, TextInput, KeyboardAvoidingView,
+    View, Text, StyleSheet, TextInput, KeyboardAvoidingView, Alert,
 }from 'react-native';
 
 import AppBar from '../components/AppBar';
@@ -13,7 +13,7 @@ export default function MemoEditScreen(){
             <View style={styles.inputContainer}>
                 <TextInput  value = '買い物リスト' multiline style={styles.input} />
             </View>
-            <CircleButton name='check' />
+            <CircleButton name='check' onPress={()=>{Alert.alert('Pressed!'); }} />
         </KeyboardAvoidingView>
     );
 }
